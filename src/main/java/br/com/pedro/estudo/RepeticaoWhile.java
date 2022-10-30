35d225e30ee8e92070da511e1163213ed9beb389
pedroquina <pquina@hotmail.com>
2022-10-30 18:40:55 -0300

    Aula sobre : While e continue Auto incremento para chave primária no banco de dados Utilziar equals para objetos e == para números

--- src/main/java/br/com/pedro/estudo/RepeticaoWhile.java
+++ src/main/java/br/com/pedro/estudo/RepeticaoWhile.java
@@ -1,11 +1,56 @@
 package br.com.pedro.estudo;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class RepeticaoWhile {
 
     public static void main(String a[]) {
 
+        exemploContinue();  
+        
+        
+
+    }
+
+
+    private static void exemploContinue(){
+        int contador = 0;
+        // continue : while, for, do-while
+        while (contador < 10) {
+
+            contador = contador + 1;
+            if ( contador % 2 != 0 ) {
+                continue;  // retorna ao while
+            }
+            
+            System.out.println("Linha : " + contador);
+        }
+        System.out.println("Fim");
+
+    }
+
+    private static void exemploString(){
+        Scanner teclado = new Scanner(System.in);
+
+        String  frase="";
+
+
+        // condição ou expressão lógica
+        // números : == , != , > , < , >= , <= 
+        // String ou Objetos :  obj.equals , ! obj.equals 
+        while (! frase.equals("sai r") ) { 
+            System.out.println("Linha : " + new Date());
+            
+            System.out.println("Para sair digite : sair ");
+            frase = teclado.nextLine();
+
+        }
+        System.out.println("Fim");        
+    }
+
+
+    private static void exemploNumerico(){
         int contador = 0;
         // condição ou expressão lógica
         while (contador < 10) {
@@ -13,7 +58,6 @@ public class RepeticaoWhile {
             contador = contador + 1;
         }
         System.out.println("Fim");
-
     }
 
 }
