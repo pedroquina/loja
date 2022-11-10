8734668df1b29595dfe50827fd92591ad2c59a4a
pedroquina <pquina@hotmail.com>
2022-11-10 19:26:27 -0300

    Exemplo de printf

---
+++ src/main/java/br/com/pedro/estudo/PrintFormat.java
@@ -0,0 +1,15 @@
+package br.com.pedro.estudo;
+
+public class PrintFormat {
+
+    public static void main(String a[]) {
+
+        String palavra = "Oi , eu sou um texto";
+        int idade = 10;
+        double salario = 123.456;
+
+        System.out.printf("Quem é você ? frase=%s  idade=%d   seu salário é %.2f   acabou",  palavra,idade,  salario);
+
+    }
+
+}
